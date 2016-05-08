19cae1cf0ab9560f87dcdbfbf8ac3fe80d2624e3
axiner <axiner.w@gmail.com>
2016-05-08 15:55:38 -0400

    trying to fix seeds

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,5 +1,5 @@
 class Guide < ActiveRecord::Base
   has_many :experiences
-  has_many :languages
+  has_and_belongs_to_many :languages
   has_many :bookings, through: :experiences
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,35 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 10.times do |i|
-  Bookings.create({
-    duration: i},
+  Booking.create(
+    duration: i,
     accepted:false,
-    experince_id:i,
+    experience_id:i,
     visitor_id:i
-  })
+  )
 
-  Catergories_experiences.create({
-    catergory_id:i,
-    experience_id:i
-  })
+  # Categories_experiences.create(
+  #   catergory_id:i,
+  #   experience_id:i
+  # )
 
-  Experience.create({
+  Experience.create(
     date:Time.now,
     description: "This is a place holder, lorem ip sum etc...",
     guide_id:i
-    })
+    )
 
-  Guide.create({
+  Guide.create(
     name: "name#{i}",
-    email:"#{i}@#{i}.#{i}"
-    password:"123456"
+    email:"#{i}@#{i}.#{i}",
+    password:"123456",
     photo:"https://robohash.org/#{i}",
     rate:5,
     city:"Toronto the Good"
-  })
-    Visitor.create({
+  )
+    Visitor.create(
       name:"name#{i}",
       email:"#{i}@#{i}.#{i}",
       password:"123456"
-    })
+    )
 end
