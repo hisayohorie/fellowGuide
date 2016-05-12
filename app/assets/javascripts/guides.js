1f303e6b2a6f767972c9b1480c044fe16a9e2815
Guin <guinifer.k@gmail.com>
2016-05-12 15:51:08 -0400

    attached experince id to booking on booked & set up sign in sign out redirect

---
+++ app/assets/javascripts/guides.js
@@ -0,0 +1,13 @@
+$(document).on('ready', function(){
+
+  // $(".experienceLink").on('click', function(e){
+  //   e.preventDefault();
+  //   $.ajax({
+  //     dataType: 'html',
+  //     method: 'get',
+  //     url: $(this).attr('href'),
+  //     data: $(this).attr('data-experience-id'),
+  //     success : function(){console.log('I am here');}
+  //   });
+  // });
+});
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,7 @@ class BookingsController < ApplicationController
     @booking.guide_id = @guide.id
     @booking.visitor_id = current_visitor.id
     @booking.accepted = false
+    binding.pry
     if @booking.save
       flash[:notice] = "Booking requested!"
       redirect_to root_url
@@ -20,6 +21,7 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @guide = Guide.find(params[:guide_id])
+    @experience_id = experience_params[:experience_id]
   end
 
   def destroy
@@ -43,7 +45,6 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     @guide = Guide.find(params[:guide_id])
-
     if @booking.update_attributes(booking_params)
       flash[:notice] = "Booking updated!"
       redirect_to guide_path(@guide)
@@ -55,7 +56,11 @@ class BookingsController < ApplicationController
 
   private
   def booking_params
-    params.require(:booking).permit(:duration, :accepted)
+    params.require(:booking).permit(:duration, :accepted, :experience_id)
+  end
+
+  def experience_params
+    params.permit(:experience_id)
   end
 
 end
