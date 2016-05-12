d381aea3ec3f0b05331a52cf34411794b80a16a7
Erin Kim <kim.erin.kim@gmail.com>
2016-05-12 16:18:40 -0400

    fixed visior's profile page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511193217) do
+ActiveRecord::Schema.define(version: 20160511200100) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "duration"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160511193217) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "email",                  default: "", null: false
     t.text     "description"
   end
 
