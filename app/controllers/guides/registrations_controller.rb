8db896799d6e313b1b1943d54773468fe6d448cc
hisayohorie <hisayohorie@gmail.com>
2016-05-17 11:24:26 -0400

    guide show edit works

--- app/controllers/guides/registrations_controller.rb
+++ app/controllers/guides/registrations_controller.rb
@@ -45,11 +45,11 @@ class Guides::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :city, :rate,:photo, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :city, :rate,:photo, {language_ids: []}, :description])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :city, :rate,:photo, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :city, :rate,:photo, {language_ids: []},:description])
   end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
