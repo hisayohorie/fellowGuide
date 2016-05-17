7f5f29b98ecebd66d2f7c91e78f3222d6c833202
hisayohorie <hisayohorie@gmail.com>
2016-05-17 15:23:13 -0400

    re-seeded, commented out guide-controller except index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,14 +72,14 @@ Visitor.destroy_all
     Language.create!({lang: language})
   end
 
-# guide1.languages.create!(lang: "Chinese")
+#guide1.languages.create!(lang: "Chinese")
 # guide2.languages.create!(lang: "Korean")
 # guide3.languages.create!(lang: "Japanese, lang: "")
 
 
-guide1.languages << (language1)
-guide2.languages << (language2)
-guide3.languages << ([language1, language3])
+ guide1.languages << (Language.find_by(lang:"Korean"))
+# guide2.languages << (language2)
+# guide3.languages << ([language1, language3])
 
 category1 = Category.create!(
 name: "Newcommer Special"
