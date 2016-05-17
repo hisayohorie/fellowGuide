d941ba41f8c226aa652cac8aa3004ef9d5bb8198
hisayohorie <hisayohorie@gmail.com>
2016-05-17 14:09:18 -0400

    language seeded,  guide show/edit works, language multiple selection not yet working

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -13,43 +13,43 @@ class GuidesController < ApplicationController
 
   end
 
-  def new
-    @guide = Guide.new
-  end
+  # def new
+  #   @guide = Guide.new
+  # end
 
   def show
     @guide = Guide.find(params[:id])
   end
 
-  def create
-    @guide = Guide.new(guide_params)
-    if @guide.save
-      flash[:notice]= "you are a guide now"
-      redirect_to guide_path(@guide)
-    else
-      render :new
-    end
-  end
-
-  def edit
-      @guide = Guide.find(params[:id])
-  end
-
-  def update
-    @guide = Guide.find(params[:id])
-    if @guide.update_attributes(guide_params)
-      flash[:notice] = "your profile is updated."
-      redirect_to guide_path(@guide)
-    else
-      render :edit
-    end
-  end
-
-  def delete
-    @guide = Guide.find(params[:id])
-    @guide.destroy
-    redirect_to root_path
-  end
+  # def create
+  #   @guide = Guide.new(guide_params)
+  #   if @guide.save
+  #     flash[:notice]= "you are a guide now"
+  #     redirect_to guide_path(@guide)
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def edit
+  #     @guide = Guide.find(params[:id])
+  # end
+  #
+  # def update
+  #   @guide = Guide.find(params[:id])
+  #   if @guide.update_attributes(guide_params)
+  #     flash[:notice] = "your profile is updated."
+  #     redirect_to guide_path(@guide)
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def delete
+  #   @guide = Guide.find(params[:id])
+  #   @guide.destroy
+  #   redirect_to root_path
+  # end
 
   private
   def guide_params
