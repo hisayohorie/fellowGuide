3b0013a531adf907df301907be142a5be1b45436
hisayohorie <hisayohorie@gmail.com>
2016-05-21 18:59:11 -0400

    reigistration controller sign up sanitizer fixed, maybe

--- app/controllers/visitors/registrations_controller.rb
+++ app/controllers/visitors/registrations_controller.rb
@@ -40,11 +40,11 @@ class Visitors::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+ protected
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation,:photo])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -63,7 +63,7 @@ class Visitors::RegistrationsController < Devise::RegistrationsController
   # end
 
   def configure_account_update_params
-   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :photo])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :photo])
  end
 
 end
